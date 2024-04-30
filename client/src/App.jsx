51e78d7cc7a2b5f688dcba5f408b36ceed07052a
Aakash-Sondagar <aakashsondar@gmail.com>
2024-04-30 22:22:13 +0530

    Restructure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-import Home from "./pages/Home/Home";
-import Product from "./pages/Product/Product";
-import Products from "./pages/Products/Products";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
 
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Slider from "../components/Slider";
+import FeaturedProducts from "../components/FeaturedProducts";
+
+const Home = () => {
+  return (
+    <>
+      <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
+    </>
+  );
+};
+
+export default Home;
